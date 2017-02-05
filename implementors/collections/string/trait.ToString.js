0d1f623a076c1d43fb6a538fbe98c755eaf2980a
Travis CI User <travis@example.org>
2017-02-05 17:53:20 +0000

    Documentation for diesel-rs/diesel@ada12e4

---
+++ implementors/collections/string/trait.ToString.js
@@ -0,0 +1,15 @@
+(function() {var implementors = {};
+implementors["chrono"] = [];
+implementors["diesel"] = [];
+implementors["libc"] = [];
+implementors["num"] = [];
+implementors["quickcheck"] = [];
+implementors["regex_syntax"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/rand/trait.Rng.js
@@ -0,0 +1,16 @@
+(function() {var implementors = {};
+implementors["chrono"] = [];
+implementors["diesel"] = [];
+implementors["libc"] = [];
+implementors["num"] = [];
+implementors["quickcheck"] = [];
+implementors["rand"] = [];
+implementors["regex_syntax"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
